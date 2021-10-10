8529efe1d41b489c08d80f150967e90ddc305117
Chengyuan-artist <1136427613@qq.com>
2021-10-10 23:44:36 +0800

    finish simple tcp sender

---
+++ include/SimpleTcpSender.h
@@ -0,0 +1,40 @@
+#ifndef SIMPLE_TCP_SENDER
+#define SIMPLE_TCP_SENDER
+
+#include "Global.h"
+#include "RdtSender.h"
+
+#define MAXN 20
+
+class SimpleTcpSender: public RdtSender {
+private:
+    int send_base;
+    int next_seq;
+    const int N; // winSize
+    const int ByteLen; // seq byte len
+    const int MOD;  // 1 << ByteLen
+
+    // used for Fast Retransmit
+    struct Ack_buf {
+        int ack_num;
+        int count = 0;
+    } ack_buf;
+
+    struct packet_buf {
+        bool is_acked = false;
+        bool is_occupied = false;
+        Packet packet;
+    } rev_buf[MAXN];
+
+
+public:
+    bool send(const Message &message);
+    void receive(const Packet &ackPkt);						//接受确认Ack，将被NetworkService调用	
+    void timeoutHandler(int seqNum);					//Timeout handler，将被NetworkService调用
+	bool getWaitingState();								//返回RdtSender是否处于等待状态，如果发送方正等待确认或者发送窗口已满，返回true
+    SimpleTcpSender(/* args */);
+    ~SimpleTcpSender();
+};
+
+
+#endif
---
+++ src/SimpleTcpSender.cpp
@@ -0,0 +1,1 @@
+#include "SimpleTcpSender.h"
